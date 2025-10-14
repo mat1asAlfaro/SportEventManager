3f98dfe24e9c37603fa4d1577279a370e28e5011
Matias Alfaro <matialfaro757575@gmail.com>
2025-10-14 16:27:51 -0300

    Add Race CRUD

---
+++ Core/IRaceRepository.cs
@@ -0,0 +1,13 @@
+using SportEventManager.Models;
+
+namespace SportEventManager.Core
+{
+  public interface IRaceRepository
+  {
+    Task<List<Race>> GetAllRacesAsync();
+    Task<Race?> GetRaceByIdAsync(int raceId);
+    Task AddRaceAsync(Race race);
+    Task UpdateRaceAsync(Race race);
+    Task DeleteRaceAsync(int raceId);
+  }
+}
