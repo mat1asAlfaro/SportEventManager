3366dd132a10c392df6294e7bd44df3fecf6f2d4
Alexis Velázquez <70240583+AVelazquez97@users.noreply.github.com>
2025-10-22 17:17:49 -0300

    Revert "Consolidate EF migrations and update dependencies"
    This reverts commit 362018d3e06bfe14eff4a6f72d01f49d3f3811a4.

--- Migrations/20251015170103_SeedData.Designer.cs
+++ Migrations/20251015170103_SeedData.Designer.cs
@@ -12,8 +12,8 @@ using SportEventManager.Data.Persistence;
 namespace SportEventManager.Migrations
 {
     [DbContext(typeof(SportEventDbContext))]
-    [Migration("20251022163930_InitialCreate")]
-    partial class InitialCreate
+    [Migration("20251015170103_SeedData")]
+    partial class SeedData
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
