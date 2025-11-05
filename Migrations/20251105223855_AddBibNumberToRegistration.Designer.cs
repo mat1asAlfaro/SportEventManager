880cb5b1a53f4b189117bd6f1d69478cc7b693cd
smate0 <m4tesilva@gmail.com>
2025-11-05 19:41:53 -0300

    feat(db): add BibNumber column to Registrations table

--- Migrations/20251105223855_AddBibNumberToRegistration.Designer.cs
+++ Migrations/20251105223855_AddBibNumberToRegistration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SportEventManager.Data.Persistence;
 
@@ -11,9 +12,11 @@ using SportEventManager.Data.Persistence;
 namespace SportEventManager.Migrations
 {
     [DbContext(typeof(SportEventDbContext))]
-    partial class SportEventDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251105223855_AddBibNumberToRegistration")]
+    partial class AddBibNumberToRegistration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20251105223855_AddBibNumberToRegistration.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SportEventManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBibNumberToRegistration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BibNumber",
+                table: "Registrations",
+                type: "nvarchar(10)",
+                maxLength: 10,
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Registrations",
+                keyColumn: "RegistrationId",
+                keyValue: 1,
+                column: "BibNumber",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Registrations",
+                keyColumn: "RegistrationId",
+                keyValue: 2,
+                column: "BibNumber",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Registrations",
+                keyColumn: "RegistrationId",
+                keyValue: 3,
+                column: "BibNumber",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Registrations",
+                keyColumn: "RegistrationId",
+                keyValue: 4,
+                column: "BibNumber",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Registrations",
+                keyColumn: "RegistrationId",
+                keyValue: 5,
+                column: "BibNumber",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Registrations",
+                keyColumn: "RegistrationId",
+                keyValue: 6,
+                column: "BibNumber",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Registrations",
+                keyColumn: "RegistrationId",
+                keyValue: 7,
+                column: "BibNumber",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Registrations",
+                keyColumn: "RegistrationId",
+                keyValue: 8,
+                column: "BibNumber",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Registrations",
+                keyColumn: "RegistrationId",
+                keyValue: 9,
+                column: "BibNumber",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Registrations",
+                keyColumn: "RegistrationId",
+                keyValue: 10,
+                column: "BibNumber",
+                value: null);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BibNumber",
+                table: "Registrations");
+        }
+    }
+}
