3c35abc513c0f192f61a213d4ab4b6b0c12b08c0
Matias Alfaro <matialfaro757575@gmail.com>
2025-11-06 07:28:59 -0300

    Add migration for BibNumber attribute in Registration model

--- Migrations/20251104025359_AtributteBibNumberRegistration.Designer.cs
+++ Migrations/20251104025359_AtributteBibNumberRegistration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SportEventManager.Data.Persistence;
 
@@ -11,9 +12,11 @@ using SportEventManager.Data.Persistence;
 namespace SportEventManager.Migrations
 {
     [DbContext(typeof(SportEventDbContext))]
-    partial class SportEventDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251104025359_AtributteBibNumberRegistration")]
+    partial class AtributteBibNumberRegistration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
