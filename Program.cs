3b4ee65ea18d2da02fb99da881033790fba798cc
Matias Alfaro <matialfaro757575@gmail.com>
2025-11-06 07:33:54 -0300

    Update project configuration and styling adjustments

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ using SportEventManager.Core;
 using SportEventManager.Data;
 using SportEventManager.Data.Persistence;
 using SportEventManager.Models;
+using Radzen;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,12 +14,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddRadzenComponents();
+
 builder.Services.AddScoped<IEventRepository, EventRepository>();
 builder.Services.AddScoped<IRaceRepository, RaceRepository>();
 builder.Services.AddScoped<IParticipantRepository, ParticipantRepository>();
 builder.Services.AddScoped<IRegistrationRepository, RegistrationRepository>();
 builder.Services.AddScoped<ISplitRepository, SplitRepository>();
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<DialogService>();
+builder.Services.AddScoped<NotificationService>();
 
 builder.Services.AddDbContextFactory<SportEventDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
